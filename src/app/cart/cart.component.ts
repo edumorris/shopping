9397b6fe263b3df2415ecb80d3ff3b07118fffa6
edumorris <eduuwachira@gmail.com>
2020-06-23 13:10:11 +0300

    Empty array function effected

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Items } from '../items';
 
 @Component({
@@ -15,30 +15,54 @@ export class CartComponent implements OnInit {
 
   totalCost: number;
 
+  private _items: Items[];
+  
+  @Input() get item(): Items[] {
+    return this._items;
+  }
+
+  set item(value: Items[]) {
+    if(value) {
+      this._items = this.items = value;
+      this.totalcalculator();
+    }
+  }
+
+  totalcalculator() {
+    this.totalCost = 0;
+    this.items.forEach((aCost: Items) => {
+      this.totalCost += aCost.cost;
+    });
+  }
+/*
   totalCalculator() {
-    /*   
+    
     for(let i = 0; i <= this.items.length; i++) {
       this.totalCost = this.totalCost + this.items[i].cost;
     }
     return this.totalCost;
-    */
+    //
    for (let i in this.items) {
      this.totalCost = this.totalCost + this.items[i].cost;
    }
 
    return this.totalCost;
   }
+  */
 
   addNewItem(itemAdd) {
     this.items.push(itemAdd);
+  }
+
+  emptyCart() {
+    confirm('Are you sure you want to empty your cart?');
+    this.items = [];
   }
 
   constructor() {
     //this.totalCost = this.totalCalculator();
    }
 
-  ngOnInit() {
-    this.totalCost = this.totalCalculator();
-  }
+  ngOnInit() {  }
 
 }
